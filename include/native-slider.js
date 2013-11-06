45f8302ca3c0c46fc257bacb0d38a7d4f3e153a0
Radoslav Sharapanov <rado@me.com>
2013-11-06 23:13:19 +0200

    hook var slider

--- include/native-slider.js
+++ include/native-slider.js
@@ -1,4 +1,5 @@
 var scrollTimer = -1;
+var slider;
 	
 function scrollslider() {
     if (scrollTimer != -1)
@@ -8,31 +9,33 @@ function scrollslider() {
 };
 
 function scrollFinished() { // center the nearest scrolling item
-	
-	$('.slider').animate ( { 'scrollLeft': Math.round ( document.getElementById('slider').scrollLeft / document.getElementById('slider').offsetWidth ) * document.getElementById('slider').offsetWidth }, 100);
+
+	$(slider).animate ( { 'scrollLeft': Math.round ( $(slider).scrollLeft() / $(slider).width() ) * $(slider).width() }, 100);
 
 }
 	
 $(document).ready(function() {
 	
+	slider = $('.slider');
+
 	/* 		To do: Fix clicking the built-in scrollbar arrow in IE  */
 	
-	{ $('.slider').on('scroll', scrollslider ); }
+	$(slider).on('scroll', scrollslider );
 	
 	$(document).keyup(function(e){
 		e.stopPropagation();
 	
 	    if (e.keyCode == 37) { // left
 	    	
-			$('.slider').stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $('.slider').scrollLeft() - $('.slider').width()}, 100, function () { 
-				$('.slider').on('scroll', scrollslider ); 
+			$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() - $(slider).width()}, 100, function () { 
+				$(slider).on('scroll', scrollslider ); 
 			});
 	       return false;
 	    }
 	    if (e.keyCode == 39) { // right
 	
-			$('.slider').stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $('.slider').scrollLeft() + $('.slider').width()}, 100, function () { 
-				$('.slider').on('scroll', scrollslider ); 
+			$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + $(slider).width()}, 100, function () { 
+				$(slider).on('scroll', scrollslider ); 
 			});
 	       return false;
 	    }
@@ -40,7 +43,7 @@ $(document).ready(function() {
 	
 	/* Initialise slider JS extras */
 	
-	$('.slider').before('<div class="slider-container"></div>').detach().appendTo('.slider-container').before('<a class="slider-arrow left">←</a>').after('<span class="slider-nav"></span>').after('<a class="slider-arrow right">→</a>');
+	$(slider).before('<div class="slider-container"></div>').detach().appendTo('.slider-container').before('<a class="slider-arrow left">←</a>').after('<span class="slider-nav"></span>').after('<a class="slider-arrow right">→</a>');
 	
 	$('.slider > *').each ( function (n) {
 	
@@ -50,17 +53,17 @@ $(document).ready(function() {
 	
 	$('.slider-arrow.left').click ( function (e) {  
 	
-		$('.slider').stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $('.slider').scrollLeft() - $('.slider').width()}, 100, function () { 
-			$('.slider').on('scroll', scrollslider ); 
+		$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() - $(slider).width()}, 100, function () { 
+			$(slider).on('scroll', scrollslider ); 
 	
 		});
 	
 	});
 	
-	$('.slider-arrow.right').click ( function (e) {  
+	$('.slider-arrow.right').click ( function (e) {
 	
-		$('.slider').stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $('.slider').scrollLeft() + $('.slider').width()}, 100, function () { 
-			$('.slider').on('scroll', scrollslider ); 
+		$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + $(slider).width()}, 100, function () { 
+			$(slider).on('scroll', scrollslider ); 
 		});
 	
 	});
@@ -68,8 +71,8 @@ $(document).ready(function() {
 	$('.slider-nav a').click ( function (e) {  
 		var n = $(this).index();
 		
-		$('.slider').stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': n * $('.slider').width() }, 100, function () { 
-			$('.slider').on('scroll', scrollslider ); 
+		$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': n * $(slider).width() }, 100, function () { 
+			$(slider).on('scroll', scrollslider );
 		});
 	
 	});
