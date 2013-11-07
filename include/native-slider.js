fdc9533310e5f15be80ac1b1766fae09690897ff
Radoslav Sharapanov <rado@me.com>
2013-11-07 12:29:37 +0200

    ...

--- include/native-slider.js
+++ include/native-slider.js
@@ -13,37 +13,42 @@ function scrollFinished() { // center the nearest scrolling item
 	$(slider).animate ( { 'scrollLeft': Math.round ( $(slider).scrollLeft() / $(slider).width() ) * $(slider).width() }, 100);
 
 }
+
+function slide(e, direction ) {
+
+	e.stopPropagation();
+    clearTimeout(scrollTimer);
+
+	$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + direction * $(slider).width() }, 100, function () { 
+		$(slider).on('scroll', scrollslider ); 
+	});
 	
+}
+
 $(document).ready(function() {
 	
 	slider = $('.slider');
 
-	/* 		To do: Fix clicking the built-in scrollbar arrow in IE  */
+	/*	To do: Fix clicking the built-in scrollbar arrow in IE  */
 	
 	$(slider).on('scroll', scrollslider );
 	
 	$(document).keyup(function(e){
-		e.stopPropagation();
-	
+		
 	    if (e.keyCode == 37) { // left
 	    	
-			$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() - $(slider).width()}, 100, function () { 
-				$(slider).on('scroll', scrollslider ); 
-			});
-	       return false;
+			slide(e, -1);
 	    }
 	    if (e.keyCode == 39) { // right
 	
-			$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + $(slider).width()}, 100, function () { 
-				$(slider).on('scroll', scrollslider ); 
-			});
-	       return false;
+			slide(e, 1);
+			
 	    }
 	});
 	
 	/* Initialise slider JS extras */
 	
-	$(slider).before('<div class="slider-container"></div>').detach().appendTo('.slider-container').before('<a class="slider-arrow left">←</a>').after('<span class="slider-nav"></span>').after('<a class="slider-arrow right">→</a>');
+	$(slider).before('<div class="slider-container"></div>').detach().appendTo('.slider-container').before('<a class="slider-arrow left">←</a>').after('<div class="slider-nav"></div>').after('<a class="slider-arrow right">→</a>');
 	
 	$('.slider > *').each ( function (n) {
 	
@@ -52,20 +57,14 @@ $(document).ready(function() {
 	});
 	
 	$('.slider-arrow.left').click ( function (e) {  
-	
-		$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() - $(slider).width()}, 100, function () { 
-			$(slider).on('scroll', scrollslider ); 
-	
-		});
-	
+		
+		slide(e, -1);
+			
 	});
 	
 	$('.slider-arrow.right').click ( function (e) {
-	
-		$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + $(slider).width()}, 100, function () { 
-			$(slider).on('scroll', scrollslider ); 
-		});
-	
+		
+		slide(e, 1);	
 	});
 	
 	$('.slider-nav a').click ( function (e) {  
