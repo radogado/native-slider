88a25f13c208c8adcf1a1942963b17881cc690ab
Radoslav Sharapanov <rado@me.com>
2013-11-08 16:30:26 +0200

    ...

--- include/native-slider.js
+++ include/native-slider.js
@@ -14,13 +14,13 @@ function scrollFinished() { // center the nearest scrolling item
 
 }
 
-function slide(e, direction ) {
+function slide(e, current_slider, direction ) {
 
 	e.stopPropagation();
     clearTimeout(scrollTimer);
 
-	$(slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(slider).scrollLeft() + direction * $(slider).width() }, 100, function () { 
-		$(slider).on('scroll', scrollslider ); 
+	$(current_slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': $(current_slider).scrollLeft() + direction * $(current_slider).width() }, 100, function () { 
+		$(current_slider).on('scroll', scrollslider ); 
 	});
 	
 }
@@ -34,14 +34,14 @@ $(document).ready(function() {
 	$(slider).on('scroll', scrollslider );
 	
 	$(document).keyup(function(e){
-		
+		/* To do: detect nearest/focused slider and control that one */
 	    if (e.keyCode == 37) { // left
 	    	
-			slide(e, -1);
+			slide(e, $('.slider'), -1);
 	    }
 	    if (e.keyCode == 39) { // right
 	
-			slide(e, 1);
+			slide(e, $('.slider'), 1);
 			
 	    }
 	});
@@ -60,21 +60,20 @@ $(document).ready(function() {
 			
 		});
 		
-		$(this).find('.slider-arrow.left').click ( function (e) {  
+		$('.slider-arrow.left').click ( function (e) {  
 			
-			slide(e, -1);
+			slide(e, $(this).siblings('.slider'), -1);
 				
 		});
 		
-		$(this).find('.slider-arrow.right').click ( function (e) {
+		$('.slider-arrow.right').click ( function (e) {
 			
-			slide(e, 1);	
+			slide(e, $(this).siblings('.slider'), 1);	
 		});
 		
-		var this_slider = this;
-		
-		$(this).find('.slider-nav a').click ( function (e) {  
+		$('.slider-nav a').click ( function (e) {  
 			var n = $(this).index();
+			var this_slider = $(this).parent().siblings('.slider');
 			
 			$(this_slider).stop( true, true ).off('scroll', scrollslider ).animate ( { 'scrollLeft': n * $(this_slider).width() }, 100, function () { 
 				$(this_slider).on('scroll', scrollslider );
