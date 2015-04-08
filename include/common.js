0e9a14dcbb3cfb4b0be267abe4d7e45cf4cdd670
radogado <rado@me.com>
2015-04-08 18:34:59 +0300

    Update from natuive 1.2.nightly

--- include/common.js
+++ include/common.js
@@ -1,100 +1,302 @@
-/* Common functions */
+function addClass_classList(el, className) {
+
+	el.classList.add(className);
+
+}
+
+function addClass_className(el, className) {
+
+    el.className += ' ' + className;
+
+}
+
+function addClass(el, className) {
+
+    if (el.classList) {
+		
+        el.classList.add(className);
+        addClass = addClass_classList;
+
+    } else {
+
+        el.className += ' ' + className;
+        addClass = addClass_className;
+
+    }
+
+}
+
+function removeClass_classList(el, className) {
+
+    el.classList.remove(className);
+
+}
+
+function removeClass_className(el, className) {
+
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+
+}
+
+function removeClass(el, className) {
+
+    if (el.classList) {
+
+        el.classList.remove(className);
+        removeClass = removeClass_classList;
+
+    } else {
+
+        removeClass_className(el, className);
+        removeClass = removeClass_className;
+
+    }
+
+}
+
+function hasClass_classList(el, className) {
+
+    return el.classList.contains(className);
+
+}
+
+function hasClass_className(el, className) {
+
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+
+}
+
+function hasClass(el, className) {
 	
-var requestAnimFrame = (function() {
-	
-	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function( callback ) {
-		window.setTimeout(callback, 1000 / 60); 
-	};
-	
-})();
-
-function addEventHandler( elem,eventType,handler ) {
-
-	if (elem.addEventListener) {
-	     elem.addEventListener (eventType,handler,false);
-	} else {
-		if (elem.attachEvent) {
-	    	elem.attachEvent ('on'+eventType,handler);
-		}
-	}     
-
-}
-
-function forEach( selector, fn ) {
-
-	elements = document.querySelectorAll(selector);
-	for (var i = 0; i < elements.length; i++) {
-		fn(elements[i], i);
-	}
-
-}
-
-function stopEvent( e ) {
- 
-	if(!e) var e = window.event;
- 
-	//e.cancelBubble is supported by IE, this will kill the bubbling process.
-	e.cancelBubble = true;
-	e.returnValue = false;
- 
-	//e.stopPropagation works only in Firefox.
-	if ( e.stopPropagation ) {
-		e.stopPropagation();
-	}
-	if ( e.preventDefault ) {
-		e.preventDefault();
-	}
- 
-	return false;
-
-}
-
-function addClass ( el, className ) { // To do: fix unnecessary spaces
-
 	if (el.classList) {
-		el.classList.add(className);
-	} else {
-		el.className += ' ' + className;
-	}
-  	
-}
-
-function removeClass ( el, className ) {
-
-	if (el.classList) {
-		el.classList.remove(className);
-	} else {
-		el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-	}
-  	
-}
-
-function hasClass ( el, className ) {
-
-	if (el.classList) {
+		
+		hasClass = hasClass_classList;
 		return el.classList.contains(className);
+
 	} else {
-		return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+		
+		hasClass = hasClass_className;
+		return hasClass_className(el, className);
+			
+	}
+	
+}
+
+function toggleClass(el, className) {
+
+    if (hasClass(el, className)) {
+
+        removeClass(el, className);
+
+    } else {
+
+        addClass(el, className);
+
+    }
+
+}
+
+function transferClass(el_origin, el_target, className) {
+
+    if (hasClass(el_origin, className)) {
+
+        addClass(el_target, className);
+
+    }
+
+}
+
+function eventElement_e(e) {
+
+    return e.target;
+
+}
+
+function eventElement_window(e) {
+
+    return window.event.srcElement;
+
+}
+
+function eventElement(e) {
+	
+	if (e) {
+		
+		eventElement = eventElement_e;
+		return e.target;
+
+	} else {
+		
+		eventElement = eventElement_window;
+		return window.event.srcElement;
+		
 	}
 
 }
 
-function thisIndex (elm) {
-    var nodes = elm.parentNode.childNodes, node;
+var parseHTML = function(str) {
+
+    tmp = document.implementation.createHTMLDocument('Parsed');
+    tmp.body.innerHTML = str;
+    return tmp.body;
+
+}
+
+function forEach(selector, fn) { // Accepts both an array and a selector
+
+    elements = (typeof selector == 'string') ? qa(selector) : selector;
+    for (var i = 0; i < elements.length; i++) {
+
+        fn(elements[i], i);
+
+    }
+
+}
+
+function addEventHandler(el, eventType, handler) {
+
+    if (el.addEventListener) {
+
+        el.addEventListener(eventType, handler, false);
+
+    } else {
+
+        if (el.attachEvent) {
+
+            el.attachEvent('on' + eventType, handler);
+
+        }
+
+    }
+
+}
+
+function removeEventHandler(el, eventType, handler) {
+
+    if (el.removeEventListener) {
+
+        el.removeEventListener(eventType, handler, false);
+
+    } else {
+
+        if (el.detachEvent) {
+
+            el.detachEvent('on' + eventType, handler);
+
+        }
+
+    }
+
+}
+
+function stopEvent(e) {
+
+    if (!e) {
+
+        if (typeof window.event == 'undefined') {
+
+            return;
+
+        }
+
+    }
+
+	if ( typeof e == 'undefined' ) {
+		
+		return false;
+
+	}
+
+    //e.cancelBubble is supported by IE, this will kill the bubbling process.
+    e.cancelBubble = true;
+    e.returnValue = false;
+
+    //e.stopPropagation works only in Firefox.
+    if (e.stopPropagation) {
+
+        e.stopPropagation();
+
+    }
+
+    if (e.preventDefault) {
+
+        e.preventDefault();
+
+    }
+
+    return false;
+
+}
+
+function thisIndex(el) {
+
+    if (!el) return;
+
+    var nodes = el.parentNode.childNodes,
+        node;
     var i = count = 0;
-    while( (node=nodes.item(i++)) && node!=elm )
-        if( node.nodeType==1 ) count++;
+
+    while ((node = nodes.item(i++)) && node != el) {
+
+        if (node.nodeType == 1) {
+
+            count++;
+
+        }
+
+    }
+
     return (count);
+
 }
 
-Math.easeInOutQuad = function ( t, b, c, d ) {
+function parentByClass(el, className) {
 
-	t /= d/2;
-	if (t < 1) {
-		return c/2*t*t + b
+    while (el.parentNode && !hasClass(el, className)) {
+
+        el = el.parentNode;
+
+    }
+	if (typeof el.tagName == 'undefined') {
+		
+		return false;
+
 	}
-	t--;
-	return -c/2 * (t*(t-2) - 1) + b;
 
-};
+    return hasClass(el, className) && el;
+
+}
+
+if (!Array.prototype.indexOf) {
+
+    Array.prototype.indexOf = function(elt) {
+
+        var len = this.length >>> 0;
+
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+        if (from < 0)
+            from += len;
+
+        for (; from < len; from++) {
+            if (from in this && this[from] === elt)
+                return from;
+        }
+        return -1;
+
+    };
+
+}
+
+function q(selector) {
+	
+	return document.querySelector(selector);
+	
+}
+
+function qa(selector) {
+	
+	return document.querySelectorAll(selector);
+	
+}
 
--- include/natuive-slider.js
+++ include/natuive-slider.js
@@ -1,303 +1,473 @@
 /* natUIve Slider */
+var new_event_support = 1;
 
-var scrollTimer = null;
-var slider;
-var original_scroll = 0;
-var slider_animation = 0;
-var touchmovex = 0;
-var current_scroll = 0;
-var height_scroll = 0;
+try { // Android Browser etc?
 
-function scrollSlider (e) {
+    test_event = new Event('t');
 
-	if ( slider_animation ) return;
+} catch (err) {
 
-	var event = e || window.event;
-	el = event.target || event.srcElement;
+    new_event_support = 0;
 
-	if ( slider != el ) { // on switching to another slider
+}
 
-		original_scroll = el.attributes['original_scroll'];
+function sliderElement(e) {
 
-	}
+    el = eventElement(e);
 
-	slider = el;
+    if (hasClass(el, 'slider-container')) {
 
-    clearTimeout(scrollTimer);
-    current_scroll = slider.scrollLeft;
-    scrollTimer = setTimeout(function() {
-		
-		if ( current_scroll == slider.scrollLeft ) { /* If all scroll, including inertia, has ended */
-		
-			slide (event, 'snap');
-		
-		}
+        return el.querySelector('.slider');
 
-    }, 50);
+    } else {
+
+        container = parentByClass(el, 'slider-container');
+        return container && container.querySelector('.slider');
+
+    }
+
+}
+
+/* Thanks to Pete & Eike Send for the swipe events – http://www.thepetedesign.com/demos/purejs_onepage_scroll_demo.html */
+
+var last_animation = 0;
+
+swipeEvents = function(el) {
+
+    var startX, startY;
+
+    el.addEventListener("touchstart", touchStart);
+
+    function touchStart(e) {
+
+        var touches = e.touches;
+        if (touches && touches.length) {
+
+            startX = touches[0].pageX;
+            startY = touches[0].pageY;
+
+            el.addEventListener("touchmove", touchMove);
+
+        }
+
+    }
+
+    function touchMove(e) {
+
+        var touches = e.touches;
+        if (touches && touches.length) {
+
+            var deltaX = startX - touches[0].pageX;
+            var deltaY = startY - touches[0].pageY;
+            delta = (Math.abs(deltaX) > Math.abs(deltaY)) ? deltaX : deltaY;
+
+            if ((hasClass(sliderElement(e), 'vertical') ? (Math.abs(deltaY) < Math.abs(deltaX)) : (Math.abs(deltaX) < Math.abs(deltaY))) && !q('.slider.lightbox')) {
+
+                return;
+
+            }
+
+            e.preventDefault();
+
+            if (Math.abs(delta) > 50) {
+
+                if (new_event_support) {
+
+                    var event = new Event((delta >= 50) ? 'swipeLeft' : 'swipeRight');
+                    el.dispatchEvent(event);
+
+                } else {
+
+                    initScroll(e, -1 * delta);
+
+                }
+
+                el.removeEventListener('touchmove', touchMove);
+
+            }
+
+        }
+    }
 
 };
 
-function moveIndex () {
+initScroll = function(event, delta) {
 
-	removeClass ( slider.parentNode.querySelector('.slider-nav a.active'), 'active' );
-	var index = Math.round( slider.scrollLeft / slider.offsetWidth ) + 1;
+    deltaOfInterest = delta;
+    timeNow = new Date().getTime();
 
-	if ( slider.parentNode.querySelector('.slider-nav').childNodes[index-1] ) {
-		addClass( slider.parentNode.querySelector('.slider-nav').childNodes[index-1], 'active');
-	}
-	
-}
+    // Cancel scroll if currently animating or within quiet period
+    if ((timeNow - last_animation) < 800) {
 
-function slideEnd () {
-
-	el.attributes['original_scroll'] = original_scroll = slider.scrollLeft;
-
-	moveIndex();
-
-	slider_animation = 0;
-
-	document.onkeyup = sliderKeyboard;
-
-	forEach('.slider', function(el, i) {
-
-		el.onscroll = scrollSlider;
-		
-	});
-	
-}
-
-function slide ( e, target ) {
-
-	if (slider_animation) return;
-
-	slider_animation = 1;
-	
-	forEach('.slider', function(el, i) {
-
-		el.onscroll = null;
-		
-	});
-	
-	var event = e || window.event; 
-
-	if ( typeof event.srcElement == 'unknown' ) { return; } // IE8
-	el = event.target || event.srcElement;
-
-	stopEvent(event);
-
-	if (typeof el == 'undefined' ) { 
-
-		el = e; 
-
-	}
-
-	var change = 0;
-
-	if ( target == 'index' ) {
-		
-		slider = el.parentNode.parentNode.querySelector('.slider');
-		start = slider.scrollLeft;
-		change = slider.children[thisIndex(el)].offsetLeft - start;
-
-	}
-	
-	if ( target == 'arrow') {
-
-		slider = el.parentNode.querySelector('.slider');
-		start = slider.scrollLeft;
-		
-		var current_index = thisIndex(slider.parentNode.querySelector('a.active'));
-
-		if ( hasClass(el, 'left') ) { // left arrow
-			
-			change = (current_index ? slider.children[ current_index-1 ].offsetLeft : 0) - slider.children[ current_index ].offsetLeft;
-
-		} else { // right arrow
-
-			change = (slider.children.length-1 > current_index) ? slider.children[ current_index+1 ].offsetLeft - slider.children[ current_index ].offsetLeft : 0;
-
-		}
-
-	}
-	
-	if ( target == 'snap') {
-
-		slider = el;
-		start = slider.scrollLeft;
-
-		if (slider.scrollLeft > original_scroll) { // Going left
-
-			change = slider.offsetWidth - slider.scrollLeft % slider.offsetWidth;
-			var current_index = Math.round( (change+start) / slider.offsetWidth );
-			if (current_index >= slider.children.length) current_index = slider.children.length-1;
-			change = slider.children[current_index].offsetLeft - slider.scrollLeft;
-
-		} else { // Going right
-
-			change = slider.scrollLeft % slider.offsetWidth - slider.offsetWidth;
-			change = -1 * (slider.offsetWidth + change);
-			var current_index = Math.round( (change+start) / slider.offsetWidth );
-			if ( current_index < 0 ) current_index = 0;
-			change = -1 * (start - slider.children[current_index].offsetLeft);
-
-		}
-		
-		if ( original_scroll == slider.scrollLeft ) {
-			
-			change = 0;
-
-		}
-
-	}
-
-	if ( !change ) {
-
-		slideEnd();
-		return;
-
-	}
-	
-	currentTime = 0,
-	increment = 20;
-	duration = 400;
-	
-	var animateScroll = function() {
-		// increment the time
-		currentTime += increment;
-		// find the value with the quadratic in-out easing function
-		var val = Math.easeInOutQuad(currentTime, start, change, duration);
-		// slide
-		slider.scrollLeft = val;
-		// do the animation unless its over
-		if( (currentTime < duration) ) {
-
-			requestAnimFrame(animateScroll);
-
-		} else {
-
-			if (slideEnd && typeof(slideEnd) === 'function') { // the animation is done so let's callback
-
-				slideEnd();
-
-			}
-
-		}
-	};
-	animateScroll();
-
-}
-
-function sliderKeyboard (e) {
-
-/*
-	slider = document.querySelector('.slider'); // Move slider #1; to do: select nearest slider
-	
-	var event = e || window.event;
-	stopEvent(event);
-    if (event.keyCode == 37) { // left
-    	
-	document.onkeyup = function (e) { return false; };
-		slide(e, 'left');
+        event.preventDefault();
+        return;
 
     }
-    if (event.keyCode == 39) { // right
 
-	document.onkeyup = function (e) { return false; };
-		slide(e, 'right');
-		
+    slide(sliderElement(event), (deltaOfInterest < 0) ? 'right' : 'left');
+
+    last_animation = timeNow;
+
+}
+
+mouseWheelHandler = function(event) {
+
+    deltaX = (event.deltaX * -10) || event.wheelDeltaX || -event.detail;
+    deltaY = (event.deltaY * -10) || event.wheelDeltaY || -event.detail;
+
+    if (Math.abs(hasClass(sliderElement(event), 'vertical') ? deltaY : deltaX) > 50) {
+
+        event.preventDefault();
+        initScroll(event, (Math.abs(deltaX) > Math.abs(deltaY)) ? deltaX : deltaY);
+
     }
-*/
+
+}
+
+function mouseEvents(el, toggle) {
+
+    if (!('onwheel' in window)) return;
+
+    if (toggle == 'off') {
+
+        el.parentNode.removeEventListener('wheel', mouseWheelHandler);
+
+    } else {
+
+        el.parentNode.addEventListener('wheel', mouseWheelHandler);
+
+    }
+
+}
+
+// Function from David Walsh: http://davidwalsh.name/css-animation-callback
+function whichAnimationEvent(){
+
+	el = document.createElement("fakeelement");
+	
+	var animations = {
+	
+		"animation"      : "animationend",
+		"OAnimation"     : "oAnimationEnd",
+		"MozAnimation"   : "animationend",
+		"WebkitAnimation": "webkitAnimationEnd"
+	
+	}
+	
+	for (t in animations){
+	
+		if (el.style[t] !== undefined){
+
+			return animations[t];
+
+		}
+	
+	}
+
+}
+
+var animationEvent = whichAnimationEvent();
+var	prefix = animationEvent == 'webkitAnimationEnd' ? '-webkit-' : ''; 
+var slide_duration = 400;
+
+function slide(el, method, index_number) {
+
+    if (window.sliderTimeout) {
+
+        clearTimeout(window.sliderTimeout);
+
+    }
+
+    var slider = parentByClass(el, 'slider-container').querySelector('.slider');
+
+	index = old_index = thisIndex(slider.parentNode.querySelector('.slider-nav a.active'));
+
+    if (method == 'index') {
+
+        index = index_number || thisIndex(el);
+    
+    }
+
+    if (method == 'right') {
+
+        if (index == (slider.children.length - 1)) {
+
+            index = 0;
+
+        } else {
+	        
+	        index++; 
+	        
+        }
+        
+    }
+
+    if (method == 'left') {
+
+        if (index == 0) {
+
+            index = slider.children.length-1;
+
+        } else {
+	        
+	        index--;
+	        
+        }
+
+    }
+
+	if (!hasClass(slider, 'vertical')) {
+		
+		slider.style.cssText = 'height: ' + slider.offsetHeight + 'px !important';
+	
+	}
+
+    removeClass(slider.parentNode.querySelector('.slider-nav .active'), 'active');
+    addClass(slider.parentNode.querySelector('.slider-nav span').children[index], 'active');
+
+    if (typeof document.body.style.transition == 'string') { // CSS transition-enabled browser...
+
+	    direction = hasClass(slider, 'vertical') ? 'translateY' : 'translateX';
+	    mouseEvents(el.parentNode, 'off');
+
+		if (!index_number) {
+	
+			addClass(q('html'), 'disable-hover');
+		
+		}
+
+		addClass(slider.children[index], 'visible');
+
+	    i = Math.min(old_index, index) - 1;
+	    while ( i++ < Math.abs(index-old_index) ) {
+	
+	        addClass(slider.children[i], 'visible');
+	
+	    }
+		
+		var styles = document.createElement('style');
+		styles.innerHTML = '@' + prefix + 'keyframes slide-index { from { ' + prefix + 'transform: ' + direction + '(' + ((index<old_index) ? (index-old_index) : 0) + '00%); } to { ' + prefix + 'transform: ' + direction + '(' + ((index<old_index) ? 0 : (index-old_index)*-1) + '00%); }}';
+		document.getElementsByTagName('head')[0].appendChild(styles);
+		addClass(styles, 'slide-index-style');
+		addClass(slider, 'slide-index');
+	
+
+        slider.addEventListener(animationEvent, function(e) {
+
+            slider.removeEventListener(animationEvent, arguments.callee);
+
+			forEach ( slider.querySelectorAll('.visible'), function (el) {
+				
+				removeClass(el, 'visible');
+				
+			});
+			addClass(slider.children[index], 'visible');
+			removeClass(slider,'slide-index');
+			slider.style.cssText = prefix + 'transform: ' + direction + '(-' + index + '00%);';
+			q('.slide-index-style').outerHTML = '';
+			
+        	removeClass(q('html'), 'disable-hover');
+            
+            if (hasClass(slider, 'lightbox')) {
+				
+				populateLightbox(slider, index);
+	            
+            }
+			document.onkeyup = sliderKeyboard;
+            mouseEvents(slider);
+
+        }, false);
+
+    } else { // ... or without animation on old browsers
+
+		slider.style.cssText = (hasClass(slider, 'vertical') ? 'top' : 'left') + ': -' + index + '00%';
+
+        if (hasClass(slider, 'lightbox')) {
+			
+			populateLightbox(slider, index);
+            
+        }
+
+    }
+
+}
+
+function sliderKeyboard(e) {
+
+    e = e || window.event;
+
+    if (typeof e == 'undefined') {
+
+        return;
+
+    }
+
+    el = e.target || e.srcElement;
+
+    if (q('.slider')) {
+
+        tag = el.tagName.toLowerCase();
+        if (tag != 'input' && tag != 'textarea') {
+
+            el = q('.slider.full-screen') || q('.slider.lightbox') || q('.slider');
+
+            switch (e.which) {
+
+                case 38:
+                	if (!hasClass(el,'vertical')) {
+	                	
+	                	return;
+
+                	}
+                case 37:
+                    slide(el, 'left');
+                    break;
+                case 40:
+                	if (!hasClass(el,'vertical')) {
+	                	
+	                	return;
+
+                	}
+                case 39:
+                    slide(el, 'right');
+                    break;
+                default:
+                    return;
+
+            }
+
+        }
+
+    }
 
 };
 
-function makeSlider (el) {
-	
-	addClass (el, 'slider');
-	el.insertAdjacentHTML('beforebegin', '<div class="slider-container"></div>'); // Create a container and move the slider in it
-	container = el.previousSibling;
-	container.insertAdjacentHTML('afterbegin', '<a class="slider-arrow left">←</a>' + el.outerHTML/* .replace( new RegExp( "\>[\n\t ]+\<" , "g" ) , "><" ) */ + '<a class="slider-arrow right">→</a><div class="slider-nav"></div>');
-	container.nextSibling.outerHTML = '';
-	el = container.querySelector('.slider');
-	
-	// Get scrollbar width and hide it by reducing the .slider-container height proportionally
+function makeSlider(el, current_slide) {
 
-	el.style.overflowX = 'hidden';
-	height_scroll = el.offsetHeight;
-	el.style.overflowX = 'scroll';
-	height_scroll = el.offsetHeight - height_scroll;
-	
-	var inlineBlockOffset = document.querySelector('.slider > *:nth-child(2)').offsetLeft - document.querySelector('.slider > *').offsetWidth;
-	
-	// Generate controls
+    if (el.children.length < 2) {
 
-	for (var i = 0; i < el.children.length; i++) {
+        return el;
+
+    }
+    addClass(el, 'slider');
+    el.insertAdjacentHTML('beforebegin', '<div class="slider-container"></div>'); // Create a container and move the slider in it
+    container = el.previousSibling;
+
+    transferClass(el, container, 'vertical');
+    transferClass(el, container, 'full-screen');
+    
+    if (hasClass(el, 'full-screen')) {
+	    
+	    addClass(q('html'), 'nooverflow');
+
+    }
+
+    container.insertAdjacentHTML('afterbegin', '<a class="slider-arrow left"></a>' + el.outerHTML + '<a class="slider-arrow right"></a><div class="slider-nav"><div><span></span></div></div>');
+    container.nextSibling.outerHTML = '';
+    el = container.querySelector('.slider');
+
+    // Generate controls
+
+    for (var i = 0; i < el.children.length; i++) {
+
+        /*
+        		// IE8 counts comments as children and produces an empty slide.			
+        		if ( el.children[i].nodeName == '#comment' ) {
+        			
+        		
+        		}
+        */
+
+        if (el.children[i].querySelector('.thumbnail')) {
+
+            slider_nav = el.parentNode.querySelector('.slider-nav');
+            addClass(slider_nav, 'thumbnails');
+            addClass(slider_nav.querySelector('span'), 'row');
+            slider_nav.querySelector('span').insertAdjacentHTML('beforeend', (!i ? '<a class="active">' : '<a>') + el.children[i].querySelector('.thumbnail').innerHTML + '</a>');
+            if (hasClass(el, 'vertical')) {
+	            
+	            addClass(el.parentNode, 'vertical-thumbnails');
+	            
+            }
+
+        } else {
+
+            container.querySelector('.slider-nav span').insertAdjacentHTML('beforeend', (!i ? '<a class="active">' : '<a>') + (i + 1) + '</a>');
+
+        }
+
+        container.querySelector('.slider-nav span').lastChild.onclick = function(e) {
+
+            slide(eventElement(e), 'index');
+
+        };
+
+    }
+
+    container.querySelector('.slider-arrow.left').onclick = function(e) {
+
+        slide(eventElement(e), 'left');
+
+    }
+
+    container.querySelector('.slider-arrow.right').onclick = function(e) {
+
+        slide(eventElement(e), 'right');
+
+    }
+
+    if (current_slide) {
+
+		slide(el, 'index', current_slide);
 		
-		if ( el.children[i].querySelector('.thumbnail') ) {
+    }
 
-			slider_nav = el.parentNode.querySelector('.slider-nav');
-			addClass( slider_nav, 'thumbnails' );
-			addClass( slider_nav, 'row' );
-			slider_nav.insertAdjacentHTML('beforeend', ( !i ? '<a class="active">' : '<a>' ) + el.children[i].querySelector('.thumbnail').innerHTML + '</a>' );
-			slider_nav.style.marginTop = (-1 * height_scroll) + 'px';
-			
-		} else {
-			
-			container.querySelector('.slider-nav').insertAdjacentHTML('beforeend', ( !i ? '<a class="active">' : '<a>' ) + (i + 1) + '</a>');
+    document.onkeyup = sliderKeyboard;
 
- 			if (!i) {
- 				
- 				container.style.height = (container.offsetHeight - height_scroll) + 'px';
- 			}
+    mouseEvents(el);
 
-		}
-		
-		container.querySelector('.slider-nav').lastChild.onclick = function (e) {
+    if ('ontouchstart' in window) {
 
-			slide(e, 'index');
+        swipeEvents(el.parentNode);
 
-		};
-		
-	}
+        el.parentNode.addEventListener("swipeLeft", function(event) {
 
-	container.querySelector('.slider-arrow.left').onclick = container.querySelector('.slider-arrow.right').onclick = function (e) {
+            el = sliderElement(event);
+            slide(el, 'right');
 
-		slide(e, 'arrow');
+        });
 
-	}
-	
-	el.onscroll = scrollSlider;
-	
-	el.attributes['original_scroll'] = 0;
-	
-	return el;
-	
+        el.parentNode.addEventListener("swipeRight", function(event) {
+
+            el = sliderElement(event);
+            slide(el, 'left');
+
+        });
+
+    }
+
+    if (el.getAttribute('data-autoslide')) { // auto slide
+
+        function autoSlide() {
+
+            slide(el, 'right');
+            window.sliderTimeout = setTimeout(autoSlide, 1000 * el.getAttribute('data-autoslide'));
+
+        }
+
+        setTimeout(autoSlide, 1000 * el.getAttribute('data-autoslide'));
+
+    }
+
+    return el;
+
 }
 
-addEventHandler ( window, 'load', function() {
+/* Start */
 
-	document.onkeyup = sliderKeyboard;
-	
-	/* Initialise JS extras: create arrows/numbers navigation */
-	forEach('.slider', function(el, i) {
+/* Initialise JS extras: create arrows/numbers navigation */
+forEach('.slider', function(el) {
 
-		makeSlider(el);
-		
-	});
-	
-	slider = document.querySelector('.slider');
-	
-	window.onresize = function () { 
-		
-		forEach('.slider', function (el,i) {
-			el.scrollLeft = 0;
-			moveIndex ();
-/* 			slide(el, 'index'); */
+    makeSlider(el);
 
- 			el.parentNode.style.height = 'auto';
- 			el.parentNode.style.height = (el.parentNode.offsetHeight - height_scroll) + 'px';
-
-		});
-		
-	}
-	
 });
